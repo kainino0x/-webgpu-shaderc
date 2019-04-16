e9a6c17f0d9ee4a5c55a019e943eddc8f7356d8e
Kai Ninomiya <kainino@chromium.org>
2019-04-15 20:10:04 -0700

    add types

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,34 @@
-type Shaderc = any;
+declare class Compiler {
+    constructor();
+    IsValid(): boolean;
+    CompileGlslToSpv(source_text: string,
+                     shader_kind: ShaderKind,
+                     input_file_name: string,
+                     entry_point_name: string,
+                     options: CompileOptions): SpvCompilationResult;
+}
+
+declare class CompileOptions {
+    constructor();
+}
+
+declare interface ShaderKind {}
+declare interface ShaderKindEnum {
+    readonly fragment: ShaderKind;
+    readonly vertex: ShaderKind;
+    readonly compute: ShaderKind;
+}
+
+declare class SpvCompilationResult {
+    GetErrorMessage(): string;
+    GetBinary(): Uint32Array;
+}
+
+declare interface Shaderc {
+    readonly Compiler: typeof Compiler;
+    readonly CompileOptions: typeof CompileOptions;
+    readonly SpvCompilationResult: typeof SpvCompilationResult;
+    readonly shader_kind: ShaderKindEnum;
+}
+
 export const instantiate: () => Promise<Shaderc>;
